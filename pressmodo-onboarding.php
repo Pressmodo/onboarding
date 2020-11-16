a3ae7270650efbe61ebf1084538a3d562ea5045a
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-11-16 11:02:06 +0000

    bootstrap the plugin

--- pressmodo-onboarding.php
+++ pressmodo-onboarding.php
@@ -8,10 +8,50 @@
  * Text Domain:     pressmodo-onboarding
  * Domain Path:     /languages
  * Version:         0.1.0
+ *
+ * @package   pressmodo-onboarding
+ * @author    Sematico LTD <hello@sematico.com>
+ * @copyright 2020 Sematico LTD
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://pressmodo.com
  */
+
+use Pressmodo\Onboarding\Plugin;
+use Pressmodo\Requirements\Requirements;
 
 defined( 'ABSPATH' ) || exit;
 
 if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require dirname( __FILE__ ) . '/vendor/autoload.php';
 }
+
+$requirements = new Requirements(
+	'Pressmodo Onboarding',
+	array(
+		'php' => '7.2',
+		'wp'  => '5.3',
+	)
+);
+
+/**
+ * Run all the checks and check if requirements has been satisfied.
+ * If not - display the admin notice and exit from the file.
+ */
+if ( ! $requirements->satisfied() ) {
+	$requirements->print_notice();
+	return;
+}
+
+/**
+ * Finally load the plugin.
+ */
+add_action(
+	'plugins_loaded',
+	function() {
+
+		$onboarding = Plugin::instance( __FILE__ );
+
+		add_action( 'plugins_loaded', array( $onboarding, 'textdomain' ), 11 );
+
+	}
+);
---
+++ src/Plugin.php
@@ -0,0 +1,151 @@
+<?php // phpcs:ignore WordPress.Files.FileName
+/**
+ * The class that loads the whole plugin after requirements have been met.
+ *
+ * @package   pressmodo-onboarding
+ * @author    Sematico LTD <hello@sematico.com>
+ * @copyright 2020 Sematico LTD
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://PM_ONBOARDING.com
+ */
+
+namespace Pressmodo\Onboarding;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Plugin class
+ */
+class Plugin {
+
+	/**
+	 * Instance of the plugin.
+	 *
+	 * @var Plugin
+	 */
+	private static $instance;
+
+	/**
+	 * Plugin file.
+	 *
+	 * @var string
+	 */
+	private $file;
+
+	/**
+	 * Setup the instance.
+	 *
+	 * @param string $file the plugin's file.
+	 * @return Plugin
+	 */
+	public static function instance( $file = '' ) {
+
+		// Return if already instantiated.
+		if ( self::isInstantiated() ) {
+			return self::$instance;
+		}
+
+		// Setup the singleton.
+		self::setupInstance( $file );
+
+		self::$instance->setupConstants();
+
+		// Return the instance.
+		return self::$instance;
+
+	}
+
+	/**
+	 * Throw error on object clone.
+	 *
+	 * The whole idea of the singleton design pattern is that there is a single
+	 * object therefore, we don't want the object to be cloned.
+	 *
+	 * @since 0.1.0
+	 * @access protected
+	 * @return void
+	 */
+	public function __clone() {
+		// Cloning instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, esc_html__( 'Cheatin&#8217; huh?', 'PM_ONBOARDING' ), '0.1.0' );
+	}
+	/**
+	 * Disable un-serializing of the class.
+	 *
+	 * @since 0.1.0
+	 * @access protected
+	 * @return void
+	 */
+	public function __wakeup() {
+		// Unserializing instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, esc_html__( 'Cheatin&#8217; huh?', 'PM_ONBOARDING' ), '0.1.0' );
+	}
+
+	/**
+	 * Return whether the main loading class has been instantiated or not.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return boolean True if instantiated. False if not.
+	 */
+	private static function isInstantiated() {
+		// Return true if instance is correct class.
+		if ( ! empty( self::$instance ) && ( self::$instance instanceof Plugin ) ) {
+			return true;
+		}
+		// Return false if not instantiated correctly.
+		return false;
+	}
+
+	/**
+	 * Helper method to setup the instance.
+	 *
+	 * @param string $file the file of the plugin.
+	 * @return void
+	 */
+	private static function setupInstance( $file = '' ) {
+		self::$instance       = new Plugin();
+		self::$instance->file = $file;
+	}
+
+	/**
+	 * Setup helper constants.
+	 *
+	 * @return void
+	 */
+	private function setupConstants() {
+
+		// Plugin version.
+		if ( ! defined( 'PM_ONBOARDING_VERSION' ) ) {
+			define( 'PM_ONBOARDING_VERSION', '1.0.0' );
+		}
+		// Plugin Root File.
+		if ( ! defined( 'PM_ONBOARDING_PLUGIN_FILE' ) ) {
+			define( 'PM_ONBOARDING_PLUGIN_FILE', $this->file );
+		}
+		// Plugin Base Name.
+		if ( ! defined( 'PM_ONBOARDING_PLUGIN_BASE' ) ) {
+			define( 'PM_ONBOARDING_PLUGIN_BASE', plugin_basename( PM_ONBOARDING_PLUGIN_FILE ) );
+		}
+		// Plugin Folder Path.
+		if ( ! defined( 'PM_ONBOARDING_PLUGIN_DIR' ) ) {
+			define( 'PM_ONBOARDING_PLUGIN_DIR', plugin_dir_path( PM_ONBOARDING_PLUGIN_FILE ) );
+		}
+		// Plugin Folder URL.
+		if ( ! defined( 'PM_ONBOARDING_PLUGIN_URL' ) ) {
+			define( 'PM_ONBOARDING_PLUGIN_URL', plugin_dir_url( PM_ONBOARDING_PLUGIN_FILE ) );
+		}
+
+	}
+
+	/**
+	 * Allow translations.
+	 *
+	 * @return void
+	 */
+	public function textdomain() {
+		load_plugin_textdomain( 'pm-onboarding', false, PM_ONBOARDING_PLUGIN_DIR . '/languages' );
+	}
+
+}
