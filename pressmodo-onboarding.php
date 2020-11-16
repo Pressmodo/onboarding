6c4fe9d71add8cb9d20efdfb1ade59293347bdde
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-11-16 10:46:45 +0000

    hello world

---
+++ pressmodo-onboarding.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Pressmodo Onboarding
+ * Plugin URI:      https://pressmodo.com
+ * Description:
+ * Author:          Alessandro Tesoro
+ * Author URI:      https://sematico.com
+ * Text Domain:     pressmodo-onboarding
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require dirname( __FILE__ ) . '/vendor/autoload.php';
+}
