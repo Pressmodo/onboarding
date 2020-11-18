ea7c967f41605267ebe8e595d2af7fa86bfff711
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-11-18 12:15:28 +0000

    prepare router

--- resources/js/react/App.js
+++ resources/js/react/App.js
@@ -1,13 +1,35 @@
 import Header from './Header'
 import OnboardingPage from './OnboardingPage'
 
-function App() {
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+} from "react-router-dom";
+
+function OnboardingApp() {
+
 	return (
-		<div class="onboard-wrapper">
-			<Header></Header>
-			<OnboardingPage></OnboardingPage>
+		<div>
+			<Switch>
+				<Route path="/onboarding/upload">
+					about page
+				</Route>
+				<Route path="/onboarding">
+					<OnboardingPage></OnboardingPage>
+				</Route>
+			</Switch>
 		</div>
 	);
 }
 
-export default App;
+export default function App() {
+	return (
+		<div className="pressmdo-onboarding-app">
+			<Header></Header>
+			<Router>
+				<OnboardingApp />
+			</Router>
+		</div>
+	);
+}
